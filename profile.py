5768edbd86aa175a317df658bafcb35cec6af00d
Mauricio Embriz <mauriembriz@Mauricios-MacBook-Pro.local>
2026-06-16 11:42:31 -0700

    Add Pyhton profile variables lab

---
+++ profile.py
@@ -0,0 +1,21 @@
+full_name = "Mauricio Embriz Gutierrez"
+nickname = "Mauri"
+age = 16
+used_pyhton_Before = False
+hobbies = ["Soccer", "Gaming", "Working out"]
+favorites = {
+    "movie": "F1 Movie",
+    "food": "quesabirria",
+    "hobby": "gaming"
+}
+stretch_goals = ["Learn to Code", "Finish Hack the Hood", "Have Fun"]
+languages_Spoken = ["English", "Spanish"]
+#print everything
+print(full_name)
+print(nickname)
+print(age)
+print(used_pyhton_Before)
+print(hobbies)
+print(favorites)
+print(stretch_goals)
+print(languages_Spoken)
